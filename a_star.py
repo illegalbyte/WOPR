98c0e28e4f1326ce574554f1b3414e506254fea8
Lewis Gentle <lewis.gentle@gmail.com>
2021-09-14 14:29:13 +1000

    for future object avoidance system.

---
+++ a_star.py
@@ -0,0 +1,65 @@
+# A* Algorithm for Missile Pathfinding
+
+import time
+
+levelWidth = 153
+levelHeight = 46
+
+
+def getNextMoves(x, y):
+	return {
+		'left':  [x-1, y],
+		'right': [x+1, y],
+		'up':  [x, y-1],
+		'down':  [x, y+1],
+		'ul': [x-1, y+1],
+		'ur': [x+1, y+1],
+		'dl': [x-1, y-1],
+		'dr': [x+1, y-1]
+	}.values()
+
+
+def getShortestPath(level, startCoordinate, endCoordinate):
+	searchPaths = [[startCoordinate]]
+	visitedCoordinates = [startCoordinate]
+
+	while searchPaths != []:
+		currentPath = searchPaths.pop(0)
+		currentCoordinate = currentPath[-1]
+
+		currentX, currentY = currentCoordinate
+
+		if currentCoordinate == endCoordinate:
+			return currentPath
+
+		for nextCoordinate in getNextMoves(currentX, currentY):
+			nextX, nextY = nextCoordinate
+
+			if nextX < 0 or nextX >= levelWidth:
+				continue
+
+			if nextY < 0 or nextY >= levelHeight:
+				continue
+
+			if nextCoordinate in visitedCoordinates:
+				continue
+
+
+
+			searchPaths.append(currentPath + [nextCoordinate])
+			visitedCoordinates += [nextCoordinate]
+
+
+'''
+shortestPath = getShortestPath(level, [1, 1], [18, 5])
+
+for coordinate in shortestPath:
+	x, y = coordinate
+	level[y][x] = '.'
+
+	for row in level:
+		print(''.join(row))
+
+	print('')
+	time.sleep(0.25)
+'''
