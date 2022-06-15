acb4753184945a93c51006507cb36ce8c4013444
Lewis Gentle <lewis.gentle@gmail.com>
2022-06-15 17:19:36 +1000

    Ocean generation works, Start of SubsAndSilos game

--- WOPR2.py
+++ WOPR2.py
@@ -1,7 +1,6 @@
 import ast
 import os
 from random import randint, choice
-from typing import Collection
 from unittest import case
 import bext
 import pyinputplus
@@ -10,7 +9,9 @@ import time
 import math
 import a_star
 import game
-
+import random
+import json
+import pickle
 
 
 # BEXT REQUIREMENTS, for printing to the screen
@@ -75,7 +76,11 @@ console_prompts = []
 # Read world map from map .txt file 
 with open('WORLD_MAP.txt', 'r') as map:
 	WORLD_MAP = map.read()
-WORLD_MAP_GRAPH = WORLD_MAP.splitlines()
+WORLD_MAP_GRAPH = WORLD_MAP.splitlines() # splits the string into a list, using "\n" as the delimiter
+WORLD_MAP_GRAPH = [list(line) for line in WORLD_MAP_GRAPH] # converts each line of the map to a list (mutable) instead of a string (immutable)
+# CHANGE RECURSION LIMIT FOR FLOOD FILL
+sys.setrecursionlimit(len(WORLD_MAP_GRAPH)*len(WORLD_MAP_GRAPH[0])*2)
+
 
 
 class draw():
@@ -231,9 +236,10 @@ class draw():
 			bext.fg('white')
 			print('-', end='')
 
+
+
 class missiles():
 	'''A bunch of functions related to firing and drawing missiles'''
-
 	
 	def get_distance(x1: int, y1: int, x2: int , y2: int) -> int:
 		'''Returns the distance between two x,y coordinates'''
@@ -312,33 +318,203 @@ class missiles():
 		draw.draw_char(STRIKE_X, STRIKE_Y-1, '🌞')
 
 
-def print_map(COLOUR: str, SPEED: float):
-	# clear entire terminal 
-	os.system('clear')
-	X = 0
-	Y = 0
-	for character in WORLD_MAP:
-		bext.goto(X, Y)
-		bext.fg(COLOUR)
-		print(character, end='')
-		time.sleep(SPEED)
-		X += 1
-		if character == '\n':
-			Y += 1
-			X = 0
-		sys.stdout.flush()
+
+class map():
+	'''Methods and attributes for the map'''
+
+	def print_map(COLOUR: str, SPEED: float, ocean=False):
+		# clear entire terminal 
+		bext.clear()
+		X = 0
+		Y = 0
+		for character in WORLD_MAP:
+			bext.goto(X, Y)
+			bext.fg(COLOUR)
+			print(character, end='')
+			time.sleep(SPEED)
+			X += 1
+			if character == '\n':
+				Y += 1
+				X = 0
+			sys.stdout.flush()
+		if ocean:
+			map.print_ocean()
 
 
-class SubsAndSilos():
+	def print_ocean(SYMBOL="^", OCEAN_COLOUR="CYAN"):
+		with open('WATER_COORDS.txt', 'r') as f:
+			xy_ocean_coords = ast.literal_eval(f.read())
+			
+		for xy in xy_ocean_coords:
+			draw.draw_char(xy[0], xy[1], SYMBOL, COLOUR=OCEAN_COLOUR)
 
-	def SetUpPlayer():
-		# pick location for player's base
-		pass
 
+	def convert_x_coord(x: str) -> int:
+		'''Converts a grid coordinate (X, 3) to a screen coordinate (100, 3)'''
+		alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+		x_values = [6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144, 150]
+		x_values_dict = dict(zip(alpha, x_values))
+		return x_values_dict[x.upper()]-2 # minus 2 due to an error offset with x_values (A is actually 4, etc) too lazy to fix rn
+
+
+	# region Dev Tool
+
+	def print_ocean_dev(MAP, TERRAIN_COLOUR="yellow", OCEAN_COLOUR="cyan"):
+		'''used for printing the ocean, but requires a pre-defined map which already displays the ocean'''
+
+		for y in range(len(MAP)):
+			for x in range(len(MAP[y])):
+				if MAP[y][x] == ' ':
+					draw.draw_char(x, y, MAP[y][x], COLOUR=TERRAIN_COLOUR)
+				elif MAP[y][x] == '^':
+					draw.draw_char(x, y, MAP[y][x], COLOUR=OCEAN_COLOUR)
+				else:
+					draw.draw_char(x, y, MAP[y][x], COLOUR=TERRAIN_COLOUR)
+				sys.stdout.flush()
+
+	def find_oceans_dev():
+		'''Tool for mapping the ocean onto the map'''
+		def draw_waters(colour="cyan", char="+"):
+			water_coords_list = []
+			with open('waters.txt', 'r') as waters:
+				for line in waters:
+					coords = ast.literal_eval(line)
+					for xy in coords:
+						water_coords_list.append(xy)
+			for xy in water_coords_list:
+				draw.draw_char(xy[0], xy[1], COLOUR=colour, CHAR=char)
+				time.sleep(0.0002)
+		
+		
+		# original sample code for finding oceans manually – superseded by flood_fill()
+		while not True:
+			try:
+				start_y += 1 # = int(xy[1])
+			except UnboundLocalError:
+				draw.clear_screen()
+				map.print_map('yellow', 0.00002)
+				draw.clear_console()  # always clear console before writing to it
+				start_y =  pyinputplus.inputInt('Enter starting y coordinate: ', min=1, max=45)
+			else:
+				MapStatus = True # initial state, is set to false to exit loop
+				while MapStatus: # main loop after initial start_y is set
+					draw.clear_screen()
+					map.print_map('yellow', 0.00002)
+					draw_waters(colour="cyan", char="^")
+					draw.clear_console()  # always clear console before writing to it
+
+					# read and draw the oceans that are already saved in the .txt file
+					draw_waters(colour="cyan", char="^")
+
+					draw.clear_console()		
+					draw.console(f"y coordinate is: {start_y}")
+					
+					start_x = convert_x_coord(pyinputplus.inputStr('Enter starting x coordinate (ABC...): ')) #int(xy[0]) #
+					draw.clear_console()
+					# check if the space is correct, otherwise prompt again
+					bext.goto(start_x, start_y)
+					draw.draw_char(start_x, start_y, CHAR='X', COLOUR="WHITE")
+					draw.clear_console()
+					if draw.get_original_character(start_x, start_y) != ' ':
+						draw.clear_console()
+						draw.console(f"The space is not empty, please try again")
+						time.sleep(0.5)
+						continue
+					correct_positioning = str(pyinputplus.inputYesNo('Is this the correct position? ')).upper()
+					if correct_positioning == "YES":
+						# check if the space to the left is a water tile aka " "
+						x_left = start_x - 1
+						while draw.get_original_character(x_left, start_y) == ' ':
+							draw.draw_char(x_left, start_y, CHAR='~', COLOUR="BLUE")
+							x_left -= 1
+							time.sleep(SMALL_PAUSE)
+						left_ocean = list(range(x_left+1, start_x))
+						# check if the space to the right is a water tile aka " "
+						x_right = start_x + 1
+						while draw.get_original_character(x_right, start_y) == ' ' and x_right < 152:
+							draw.draw_char(x_right, start_y, CHAR='~', COLOUR="BLUE")
+							x_right += 1
+							time.sleep(SMALL_PAUSE)
+						right_ocean = list(range(start_x, x_right)) # +1 because it is off by 1
+
+						new_ocean_tiles_list = left_ocean + right_ocean
+
+						# completely redraw the new ocean and check if its correct?
+						map.print_map('yellow', 0.00002)
+						draw.clear_console()
+						for x in new_ocean_tiles_list:
+							draw.draw_char(x, start_y, CHAR='~', COLOUR="BLUE")
+							time.sleep(SMALL_PAUSE)
+
+						draw.clear_console()
+						correctly_drawn = str(pyinputplus.inputYesNo('Is this the correct ocean? ')).upper()
+						if correctly_drawn == "YES":
+							ocean_tiles_coords = []
+							for x in new_ocean_tiles_list:
+								ocean_tiles_coords.append((x, start_y))
+							with open('waters.txt', 'a') as file:
+								file.write(str(ocean_tiles_coords))
+								file.write('\n')
+								file.close()
+							draw.clear_console()
+							print('Ocean saved!')
+							time.sleep(SMALL_PAUSE)
+						else:
+							continue
+
+						# ask dev if they want to continue marking oceans, else quit
+						more_maps = pyinputplus.inputMenu(["Yes", "No"], 'Do you want to save more maps?\n', numbered=True)
+						draw.console(more_maps)
+
+						draw.clear_console()
+
+						if more_maps == "Yes":
+							start_y += 1
+							continue
+						else:
+							MapStatus = False
+					else:
+						continue
+
+		def flood_fill(matrix, x, y, old, new):
+			# we need the x and y of the start position, the old value,
+			# and the new value
+			# the flood fill has 4 parts
+			# firstly, make sure the x and y are inbounds
+			if x < 0 or x >= len(matrix[0]) or y < 1 or y >= len(matrix)-1:
+				return
+			# secondly, check if the current position equals the old value
+			if matrix[y][x] != old:
+				return
+
+			# thirdly, set the current position to the new value
+			matrix[y][x] = new
+			# fourthly, attempt to fill the neighboring positions
+			flood_fill(matrix, x+1, y, old, new)
+			flood_fill(matrix, x-1, y, old, new)
+			flood_fill(matrix, x, y+1, old, new)
+			flood_fill(matrix, x, y-1, old, new)
+			# return the matrix
+			return matrix
+
+
+		ocean_map = flood_fill(WORLD_MAP_GRAPH, 110, 28, " ", "^")
+		WATER_COORDS = [] 
+		for y in range(len(ocean_map)):
+			for x in range(len(ocean_map[y])):
+				if ocean_map[y][x] == "^":
+					WATER_COORDS.append((x, y))
+		with open("WATER_COORDS.txt", "a") as f:
+			f.write(str(WATER_COORDS))
+	# endregion
+
+
+
+# region Classic Mode Code
 
 def classic_mode():
 	bext.clear()
-	print_map("yellow", REFRESH_RATE)
+	map.print_map("yellow", REFRESH_RATE)
 	# create list of countries / players: 
 	unassigned_countries = []
 
@@ -486,131 +662,65 @@ def classic_mode():
 		base_message(f'YOU WON IN {turn} TURNS, RESTARTING IN 10s', player_country)
 
 
-
-def convert_x_coord(x: str):
-	'''Converts a grid coordinate (X, 3) to a screen coordinate (100, 3)'''
-	alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-	x_values = [6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144, 150]
-	x_values_dict = dict(zip(alpha, x_values))
-	return x_values_dict[x.upper()]-2
-	
+# endregion
 
 
-def find_oceans_dev():
-	'''Tool for mapping the ocean onto the map'''
-	def draw_waters(colour="cyan", char="+"):
-		water_coords_list = []
-		with open('waters.txt', 'r') as waters:
-			for line in waters:
-				coords = ast.literal_eval(line)
-				for xy in coords:
-					water_coords_list.append(xy)
-		for xy in water_coords_list:
-			draw.draw_char(xy[0], xy[1], COLOUR=colour, CHAR=char)
-			time.sleep(0.0002)
-	
-	# starting_coordinates = []
-	# for x in ["R"]:
-	# 	x = convert_x_coord(x)
-	# 	for y in range(18, 43):
-	# 		starting_coordinates.append((x, y))
-			
-	# for xy in starting_coordinates:
-	
+class SubsAndSilos():
 
-	while True:
-		try:
-			start_y += 1 # = int(xy[1])
-		except UnboundLocalError:
-			draw.clear_screen()
-			print_map('yellow', 0.00002)
-			draw.clear_console()  # always clear console before writing to it
-			start_y =  pyinputplus.inputInt('Enter starting y coordinate: ', min=1, max=45)
-		else:
-			MapStatus = True # initial state, is set to false to exit loop
-			while MapStatus: # main loop after initial start_y is set
-				draw.clear_screen()
-				print_map('yellow', 0.00002)
-				draw_waters(colour="cyan", char="^")
-				draw.clear_console()  # always clear console before writing to it
-
-				# read and draw the oceans that are already saved in the .txt file
-				draw_waters(colour="cyan", char="^")
-
-				draw.clear_console()		
-				draw.console(f"y coordinate is: {start_y}")
-				
-				start_x = convert_x_coord(pyinputplus.inputStr('Enter starting x coordinate (ABC...): ')) #int(xy[0]) #
-				draw.clear_console()
-				# check if the space is correct, otherwise prompt again
-				bext.goto(start_x, start_y)
-				draw.draw_char(start_x, start_y, CHAR='X', COLOUR="WHITE")
-				draw.clear_console()
-				if draw.get_original_character(start_x, start_y) != ' ':
-					draw.clear_console()
-					draw.console(f"The space is not empty, please try again")
-					time.sleep(0.5)
-					continue
-				correct_positioning = str(pyinputplus.inputYesNo('Is this the correct position? ')).upper()
-				if correct_positioning == "YES":
-					# check if the space to the left is a water tile aka " "
-					x_left = start_x - 1
-					while draw.get_original_character(x_left, start_y) == ' ':
-						draw.draw_char(x_left, start_y, CHAR='~', COLOUR="BLUE")
-						x_left -= 1
-						time.sleep(SMALL_PAUSE)
-					left_ocean = list(range(x_left+1, start_x))
-					# check if the space to the right is a water tile aka " "
-					x_right = start_x + 1
-					while draw.get_original_character(x_right, start_y) == ' ' and x_right < 152:
-						draw.draw_char(x_right, start_y, CHAR='~', COLOUR="BLUE")
-						x_right += 1
-						time.sleep(SMALL_PAUSE)
-					right_ocean = list(range(start_x, x_right)) # +1 because it is off by 1
-
-					new_ocean_tiles_list = left_ocean + right_ocean
-
-					# completely redraw the new ocean and check if its correct?
-					print_map('yellow', 0.00002)
-					draw.clear_console()
-					for x in new_ocean_tiles_list:
-						draw.draw_char(x, start_y, CHAR='~', COLOUR="BLUE")
-						time.sleep(SMALL_PAUSE)
-
-					draw.clear_console()
-					correctly_drawn = str(pyinputplus.inputYesNo('Is this the correct ocean? ')).upper()
-					if correctly_drawn == "YES":
-						ocean_tiles_coords = []
-						for x in new_ocean_tiles_list:
-							ocean_tiles_coords.append((x, start_y))
-						with open('waters.txt', 'a') as file:
-							file.write(str(ocean_tiles_coords))
-							file.write('\n')
-							file.close()
-						draw.clear_console()
-						print('Ocean saved!')
-						time.sleep(SMALL_PAUSE)
-					else:
-						continue
-
-					# ask dev if they want to continue marking oceans, else quit
-					more_maps = pyinputplus.inputMenu(["Yes", "No"], 'Do you want to save more maps?\n', numbered=True)
-					draw.console(more_maps)
-
-					draw.clear_console()
-
-					if more_maps == "Yes":
-						start_y += 1
-						continue
-					else:
-						MapStatus = False
-				else:
-					continue
+	def display_map():
+		'''Display the map of the game'''
+		bext.clear()
+		map.print_map("yellow", SPEED=REFRESH_RATE, ocean=True)
+		draw.clear_console()
+		
+	def ask_for_ocean_coords():
+		'''Ask the user for the coordinates of the ocean'''
+		x = map.convert_x_coord(pyinputplus.inputStr(prompt='Enter an X coordinate in the ocean (A, B, C, etc): '))
+		y = pyinputplus.inputInt("Enter a Y coordinate in the ocean (1, 2, 3, etc): ", min=1, max=45)
+		return (x, y)
 
 
 
+	def setup_game():
+		
+		unassigned_countries = [] # create list of countries / players: 
+		for country in countries:
+			# reset status of all countries:
+			countries[country]['status'] = True
+			unassigned_countries.append(country)
+		all_countries = unassigned_countries.copy()
 
+		# ask what country to play as
+		player_country = pyinputplus.inputMenu(unassigned_countries, "CHOOSE YOUR COUNTRY:\n", lettered=True)
+		draw.clear_console()
 
+		# ask how many players
+		num_players = pyinputplus.inputInt(prompt="How many other (CPU) players? (1-4) > ", min=1, max=4)
+
+		player_sub_x, player_sub_y = SubsAndSilos.ask_for_ocean_coords()
+
+	def setup_countries():
+		pass
+
+	def setup_submarines():
+		pass
+
+	def setup_silos():
+		pass
+
+	def setup_missiles():
+		pass
+
+	def setup_player():
+		pass
+
+	def setup_enemies():
+		pass
+
+	def start():
+		pass
+
+		
 
 def submarinesandsilos():
 	'''
@@ -619,21 +729,23 @@ def submarinesandsilos():
 		It is responsible for setting up the game, and calling the functions
 		that handle the game logic.
 	'''
+	# display map
+	SubsAndSilos.display_map()
 	# set up the game
-	setup_game()
+	SubsAndSilos.setup_game()
 	# set up the countries
-	setup_countries()
+	SubsAndSilos.setup_countries()
 	# set up the submarines
-	setup_submarines()
+	SubsAndSilos.setup_submarines()
 	# set up the silos
-	setup_silos()
+	SubsAndSilos.setup_silos()
 	# set up the missiles
-	setup_missiles()
+	SubsAndSilos.setup_missiles()
 	# set up the player
-	setup_player()
+	SubsAndSilos.setup_player()
 	# set up the enemies
-	setup_enemies()
-	# set up the unassigned countries
+	SubsAndSilos.setup_enemies()
+	'''	# set up the unassigned countries
 	setup_unassigned_countries()
 	# set up the original allies and enemies
 	setup_original_allies_and_enemies()
@@ -655,9 +767,9 @@ def submarinesandsilos():
 	setup_original_player()
 	# set up the original countries
 	setup_original_countries()
-
+	'''
 	# start the game
-	submarinesandsilos_game()
+	SubsAndSilos.start()
 
 
 
@@ -678,7 +790,7 @@ def main():
 	while True:
 		# Runs at the start of each game
 		draw.clear_console()
-		find_oceans_dev()
+		submarinesandsilos()
 		sys.exit()
 
 
